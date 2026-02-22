f2760a99b95157bed86fc4fad00c09b69255d1ef
prince kumar <princekumar@princes-MacBook-Air.local>
2026-02-23 04:10:26 +0530

    Fix package-lock.json sync and login page layout issue

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -2,13 +2,23 @@
 import '@/styles/globals.css'
 import { AuthProvider } from '@/context/AuthContext'
 import Layout from '@/components/Layout'
+import { useRouter } from 'next/router'
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter()
+  
+  // Don't show Layout on login page
+  const showLayout = router.pathname !== '/login' && router.pathname !== '/'
+  
   return (
     <AuthProvider>
-      <Layout>
+      {showLayout ? (
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      ) : (
         <Component {...pageProps} />
-      </Layout>
+      )}
     </AuthProvider>
   )
 }
